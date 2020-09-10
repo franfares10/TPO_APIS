aaecee06355301b682c0a8a04c1595ba3b1d2e70
LucasRial <lucasrial@outlook.com.ar>
2020-09-10 14:42:04 -0300

    v1.2.3 - Arreglada view Encuestas (components -> Dashboard -> dashboard.js)

--- responder-encuestas/src/views/Dashboard/Dashboard.js
+++ responder-encuestas/src/views/Dashboard/Dashboard.js
@@ -2,6 +2,9 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/material-dashboard-react/encuestasStyle.js";
 import CardHeader from "components/Card/CardHeader";
+import GridContainer from "components/Grid/GridContainer";
+import GridItem from "components/Grid/GridItem";
+import Button from "components/CustomButtons/Button";
 
 const useStyles = makeStyles(styles);
 
@@ -9,16 +12,21 @@ export default function Dashboard() {
   const classes = useStyles();
   return (
     <div>
-      <CardHeader color="primary">
-        <h4 className={classes.cardTitleWhite}>Encuestas por responder</h4>
-      </CardHeader>
+    <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+                <CardHeader color="primary">
+                    <h4 className={classes.cardTitleWhite}>Encuestas por responder</h4>
+                </CardHeader>
+        </GridItem>
+    </GridContainer>
+    <GridContainer justify={"center"}>
       <div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
             <div className ={classes.infoEncuestaCard}>
                 <h4><b>Encuesta Financiera</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
       </div>
         <div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
@@ -26,7 +34,7 @@ export default function Dashboard() {
                 <h4><b>Encuesta de Procesos</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
         </div>
         <div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
@@ -34,7 +42,7 @@ export default function Dashboard() {
                 <h4><b>Encuesta de Logística</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
         </div>
         <div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
@@ -42,7 +50,7 @@ export default function Dashboard() {
                 <h4><b>Encuesta Financiera</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
       </div>
         <div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
@@ -50,7 +58,7 @@ export default function Dashboard() {
                 <h4><b>Encuesta de Procesos</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
         </div>
         <div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
@@ -58,14 +66,14 @@ export default function Dashboard() {
                 <h4><b>Encuesta de Logística</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
         </div><div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
             <div className ={classes.infoEncuestaCard}>
                 <h4><b>Encuesta Financiera</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
       </div>
         <div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
@@ -73,7 +81,7 @@ export default function Dashboard() {
                 <h4><b>Encuesta de Procesos</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
         </div>
         <div className={classes.encuestaCard}>
             <img src="https://cdn.smassets.net/assets/cms/cc/uploads/satisfaction-question-survey-question-types.png"></img>
@@ -81,8 +89,9 @@ export default function Dashboard() {
                 <h4><b>Encuesta de Logística</b></h4>
                 <p>Tiempo Restante: DD-HH:mm</p>
             </div>
-            <button>RESPONDER ENCUESTA</button>
+            <Button color="primary">RESPONDER ENCUESTA</Button>
         </div>
+        </GridContainer>
       </div>
   );
 }
