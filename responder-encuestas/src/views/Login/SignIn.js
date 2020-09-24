5581f95d32ba60aa2988f71628ea7b4a2eff69d1
Fran Fares <franciscofares23@gmail.com>
2020-09-24 18:37:21 -0300

    cambios que hizo lucas en el login

--- responder-encuestas/src/views/Login/SignIn.js
+++ responder-encuestas/src/views/Login/SignIn.js
@@ -1,59 +1,56 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, styled, withStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, styled, withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import history from "utils/History/history";
-import ItemCardImagen from "components/LoginComponents/ItemCardImagen"
+import ItemCardImagen from "components/LoginComponents/ItemCardImagen";
 
 import avatar from "assets/img/loguito.png";
-import { whiteColor } from 'assets/jss/material-dashboard-react';
-import { blue } from '@material-ui/core/colors';
+import { whiteColor } from "assets/jss/material-dashboard-react";
+import { blue } from "@material-ui/core/colors";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-    
-        Your Website
-    {' '}
-      {new Date().getFullYear()}
-      {'.'}
+      {"Copyright © "}
+      Your Website {new Date().getFullYear()}
+      {"."}
     </Typography>
   );
 }
 const pruebaObservatorio = {
-  'user': 'paula',
-  'contra': 'sarasa'
-}
+  user: "paula",
+  contra: "sarasa",
+};
 
 const pruebaEmpresa = {
-  'user': 'tomas',
-  'contra': 'malio'
-}
+  user: "tomas",
+  contra: "malio",
+};
 const styles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
-  fondo:{
-      backgroundColor: '#FFFFFF'
+  fondo: {
+    backgroundColor: "#FFFFFF",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: whiteColor,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -62,25 +59,27 @@ const styles = makeStyles((theme) => ({
 }));
 
 const submitPressed = (event) => {
-  event.preventDefault()
+  event.preventDefault();
 
   let usuario = event.target.email.value;
   let password = event.target.password.value;
 
-  if (usuario === pruebaObservatorio.user && password === pruebaObservatorio.contra) {
-    history.push('/admin/dashboard')
+  if (
+    usuario === pruebaObservatorio.user &&
+    password === pruebaObservatorio.contra
+  ) {
+    history.push("/admin/dashboard");
   }
   if (usuario === pruebaEmpresa.user && password === pruebaEmpresa.contra) {
-    history.push('/companyAdmin/dashboard')
+    history.push("/companyAdmin/dashboard");
   }
-}
+};
 
-const SignUpPressed = () =>{
-  history.push("/signin")
-}
+const SignUpPressed = () => {
+  history.push("/signin");
+};
 
 class SignIn extends React.Component {
-
   constructor(props) {
     super(props);
   }
@@ -89,73 +88,72 @@ class SignIn extends React.Component {
     const { classes } = this.props;
 
     return (
-      <div classname={classes.fondo}>
-      <Container component="main" maxWidth="xs" >
-        <CssBaseline />
-        <div className={classes.paper}>
-        <div className="form-group" align="center" >
-            <img src={avatar} width="130" height="120" align="center"></img>
-        </div>
-          <form className={classes.form} noValidate onSubmit={submitPressed} onSignUp={SignUpPressed}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="#187C7C" />}
-              label="Remember me"
-            />
-
-
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="#187C7C" 
-              className={classes.submit}
+      <div className={classes.fondo}>
+        <Container component="main" maxWidth="xs" align="center">
+          <CssBaseline />
+          <div className={classes.paper}>
+            <div className="form-group" align="center">
+              <img src={avatar} width="130" height="120" align="center"></img>
+            </div>
+            <form
+              className={classes.form}
+              noValidate
+              onSubmit={submitPressed}
+              onSignUp={SignUpPressed}
             >
-              Sign In
-                  </Button>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="#187C7C" />}
+                label="Remember me"
+              />
 
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="#187C7C"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
 
-
-            <Grid container >
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                    </Link>
-              </Grid>
-              <Grid item onClick ={
-                () => SignUpPressed()
-              }>    
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item onClick={() => SignUpPressed()}>
                   {"Don't have an account? Solicita tu Cuenta"}
+                </Grid>
               </Grid>
-            </Grid>
-          </form>
-        </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
+            </form>
+          </div>
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+        </Container>
       </div>
     );
   }
